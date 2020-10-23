cc679d334c3a84f1d91722cbecec79cbfb8613f6
Vincenzo Navarra <54999963+vinnav@users.noreply.github.com>
2020-10-23 13:20:27 +0200

    add task update function

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,6 +1,7 @@
 var Task = require('../models/task');
 var Caseobj = require('../models/caseobj');
 const {body,validationResult} = require('express-validator');
+var async = require('async')
 
 // Display list of all tasks.
 exports.task_list = function(req, res, next) {
@@ -84,7 +85,7 @@ exports.task_create_post = [
 ];
 
 // Display task delete form on GET.
-exports.task_delete_get = function(req, res) {    
+exports.task_delete_get = function(req, res, next) {    
     Task.findById(req.params.id)
     .populate('caseobj')
     .exec(function (err, task) {
@@ -95,7 +96,7 @@ exports.task_delete_get = function(req, res) {
 }
 
 // Handle task delete on POST.
-exports.task_delete_post = function(req, res) {
+exports.task_delete_post = function(req, res, next) {
     Task.findById(req.body.taskid)
     .exec(function (err, task) {
         if (err) { return next(err); }
@@ -109,11 +110,71 @@ exports.task_delete_post = function(req, res) {
 };
 
 // Display task update form on GET.
-exports.task_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: task update GET');
+exports.task_update_get = function(req, res, next) {
+
+    async.parallel({
+        tasks: function(callback){
+            Task.findById(req.params.id)
+            .exec(callback);
+        },
+        caseobj_list: function(callback){
+            Caseobj.find({},'name client')
+            .populate('client')
+            .exec(callback);
+        },
+    }, function(err,results){
+        if(err){return next(err);}
+        res.render('task_form', {title: 'Update Task', task: results.tasks, caseobj_list: results.caseobj_list});
+    });
 };
 
 // Handle task update on POST.
-exports.task_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: task update POST');
-};
+exports.task_update_post = [
+
+    // Validate and sanitise fields.
+    body('description', 'Description must be specified').trim().isLength({ min: 1 }).escape(),
+    body('due', 'Invalid date').isISO8601().toDate(),
+    
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a BookInstance object with escaped and trimmed data.
+        var task = new Task(
+          { caseobj: req.body.caseobj,
+            description: req.body.description,
+            status: req.body.status,
+            due: req.body.due,
+            _id: req.params.id
+           });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values and error messages.
+            async.parallel({
+                tasks: function(callback){
+                    Task.findById(req.params.id)
+                    .exec(callback);
+                },
+                caseobj_list: function(callback){
+                    Caseobj.find({},'name client')
+                    .populate('client')
+                    .exec(callback);
+                },function(err,results){
+                    if(err){return next(err);}
+                    // Successful, so render.
+                    res.render('task_form', { title: 'Create Task', caseobj_list: caseobj, selected_caseobj: task.caseobj._id , errors: errors.array(), task: task });
+            }});
+                return
+            }
+            else {
+            // Data from form is valid.
+            Task.findByIdAndUpdate(req.params.id, task, {}, function (err, thetask) {
+                if (err) { return next(err); }
+                   // Successful - redirect to new record.
+                   res.redirect(thetask.url);
+                });
+        }
+    }
+];
